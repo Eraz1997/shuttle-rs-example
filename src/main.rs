4b09575555743ffd47b733f55aa098c954b55ada
Enrico Schifano <enrs@bendingspoons.com>
2024-09-23 09:39:59 +0200

    feat: scaffold db and endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
-use axum::{routing::get, Router};
+use axum::extract::Json;
+use axum::extract::Path;
+use axum::routing::post;
+use axum::{routing::get, Extension, Router};
+use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
 
-async fn hello_world() -> &'static str {
+#[derive(Serialize, Deserialize)]
+struct RequestPayload {
+    url: String,
+}
+
+async fn redirect(Path(id): Path<String>, Extension(pool): Extension<PgPool>) -> &'static str {
+    "Hello, world!"
+}
+
+async fn shorten_url(
+    Extension(pool): Extension<PgPool>,
+    Json(payload): Json<RequestPayload>,
+) -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+        .route("/:id", get(redirect))
+        .route("/", post(shorten_url))
+        .layer(Extension(pool));
 
     Ok(router.into())
 }
